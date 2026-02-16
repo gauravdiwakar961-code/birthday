2e33705d2cac9dd537e4e82fc43cdc6853ff7974
gauravdiwakar961-code <gauravdiwakar961@gmail.com>
2026-02-16 13:11:02 +0530

    Add files via upload

---
+++ app (4).py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Title
+st.title("🎂 Birthday Celebration App 🎂")
+
+# Inputs
+name = st.text_input("Enter the Birthday Girl's Name")
+age = st.number_input("Enter Age", min_value=1, step=1)
+
+# Button
+if st.button("Celebrate 🎉"):
+    st.success(f"🎉 Happy {age} Birthday, {name}! 🎂")
+    st.balloons()
